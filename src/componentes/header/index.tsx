d6f2d45a717f1180127e751004f32dc7db733185
dkimisima@gmail.com <dkimisima@gmail.com>
2024-10-22 10:47:02 -0300

    add routes layout

---
+++ src/componentes/header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainerer } from "./styles";
+
+export function Header() {
+    return (
+        <HeaderContainerer>
+            <span>Logo</span>
+            <nav>
+                <a href="">timer</a>
+                <a href="">History</a>
+            </nav>
+        </HeaderContainerer>
+    )
+}
